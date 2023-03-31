ce5e65677adf97f9368eb6eea6afcb00f9c8ee72
Uwe Wieben <u.wieben@cloud-user-skills.de>
2023-03-31 23:13:48 +0200

    ContentWindow Layouting

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -1,7 +1,7 @@
-import ContentWindow from "../_layout/ContentWindow";
+﻿import ContentWindow from "../_layout/ContentWindow";
 
 export default function Contact() {
 	return (
-		<ContentWindow title="Kontakt">gfkglfjdsj kgfldsj gklfd�js gklfdjs gkfldsj gkfldjkd</ContentWindow>
+		<ContentWindow title="Kontakt">gfkglfjdsj kgfldsj gklfdöjs gklfdjs gkfldsj gkfldjkd</ContentWindow>
 	)
 }
--- src/privacy/Privacy.js
+++ src/privacy/Privacy.js
@@ -1,7 +1,7 @@
-import ContentWindow from "../_layout/ContentWindow";
+﻿import ContentWindow from "../_layout/ContentWindow";
 
 export default function Privacy() {
 	return (
-		<ContentWindow title="Datenschutz">gfkglfjdsj kgfldsj gklfd�js gklfdjs gkfldsj gkfldjkd</ContentWindow>
+		<ContentWindow title="Datenschutz">gfkglfjdsj kgfldsj gklfdöjs gklfdjs gkfldsj gkfldjkd</ContentWindow>
 	)
 }
