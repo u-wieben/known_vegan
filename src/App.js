85e3f89570d825eae72add0ed24a4fd2bb5656ce
Uwe Wieben <u.wieben@cloud-user-skills.de>
2023-04-02 08:55:19 +0200

    Cookies Banner (ohne Consent-Tool) Filter MockData angepasst

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./_layout/css/styles.css"; 
+
+import { Navigate, Route, Routes } from "../node_modules/react-router-dom/dist/index";
+import CookieHandler from 'universal-cookie';
+
 import FooterNav from "./_layout/FooterNav";
 import HeaderNav from "./_layout/HeaderNav";
 import Hero from "./hero/Hero";
-import { Route, Routes } from "../node_modules/react-router-dom/dist/index";
 import Content from "./_layout/Content";
 import Contact from "./contact/Contact";
 import Cookies from "./cookies/Cookies";
@@ -11,13 +14,19 @@ import NewRecipe from "./newrecipe/NewRecipe";
 import Page404 from "./error/Page404";
 import Filter from "./filter/Filter";
 
-function App() { 
-  return (<>
-    <HeaderNav />
+const cookies = new CookieHandler(); 
+const userToken = cookies.get("token"); 
+
+export default function App() { 
+  console.log("APP Aufruf"); 
+  return (
+    <>
+      { console.log("APP: Vor HeaderNav") }
+      <HeaderNav />
       <Content>
         <Routes>
           <Route path="/">
-            <Route index element={<Hero /> }/> 
+            <Route index element={<Hero />} />
             <Route path="impressum" element={<Contact />} /> 
             <Route path="cookies" element={<Cookies />} /> 
             <Route path="datenschutz" index element={<Privacy />} /> 
@@ -26,9 +35,10 @@ function App() {
             <Route path="*" index element={<Page404 />} /> 
           </Route>
         </Routes>
+        {!userToken && <Navigate to="/cookies" />}
       </Content>
-    <FooterNav />
-  </>);
+      <FooterNav />
+      {console.log("APP: Nach FooterNav")}
+    </>
+  );
 }
-
-export default App;
--- src/_layout/Content.js
+++ src/_layout/Content.js
@@ -1,6 +1,7 @@
 export default function Content({children}) {
 	return (<>
 		<main className="d-flex justify-content-around align-items-center container-fluid flex-grow-1 overflow-auto position-relative">
+			{console.log("2222")}
 			{children}
 		</main>
 	</>); 
